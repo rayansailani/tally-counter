ce1f7526dfb631c14296eb8ca01a22647f86a282
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2022-04-09 11:22:50 +0530

    added local storage feature

--- src/App.js
+++ src/App.js
@@ -1,79 +1,11 @@
-import React, { useState } from "react";
-import { Box, Typography, Container, IconButton, Button } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
+import React from "react";
+import { Container } from "@mui/material";
+import Counter from "./counter";
 import "./styles.css";
 const App = () => {
-  const [count, setCount] = useState(0);
-  const incrementCount = () => {
-    setCount((prev) => prev + 1);
-  };
-  const resetCount = () => {
-    setCount(0);
-  };
-  const decrementCount = () => {
-    if (count >= 1) setCount((prev) => prev - 1);
-  };
   return (
     <Container maxWidth="xl" sx={{ background: "#44FFD1", minWidth: "100vw" }}>
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-      >
-        <Typography variant="h3" gutterBottom>
-          Tally Counter
-        </Typography>
-        <Typography
-          variant="h3"
-          gutterBottom
-          sx={{ margin: "3rem 0 !important", fontWeight: "bold" }}
-        >
-          {count}
-        </Typography>
-        {/* <Box
-          display="flex"
-          justifyContent="space-between"
-          width="50vw"
-          alignItems="center"
-          marginBottom="1rem"
-        >
-          <IconButton size="large" onClick={incrementCount}>
-            <AddIcon sx={{ fontSize: "6rem" }} />
-          </IconButton>
-          <IconButton size="large" onClick={decrementCount}>
-            <RemoveIcon sx={{ fontSize: "6rem" }} />
-          </IconButton>
-        </Box> */}
-        <Button
-          variant="contained"
-          sx={{ width: "60vw", height: "40vh" }}
-          onClick={incrementCount}
-        >
-          <AddIcon sx={{ fontSize: "6rem" }} />
-        </Button>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          width="50vw"
-          alignItems="center"
-          marginBottom="1rem"
-        >
-          <Button
-            size="large"
-            onClick={resetCount}
-            variant="contained"
-            sx={{ height: "3rem", margin: "3rem 0 !important" }}
-          >
-            Reset
-          </Button>
-          <IconButton size="large" onClick={decrementCount}>
-            <RemoveIcon sx={{ fontSize: "6rem" }} />
-          </IconButton>
-        </Box>
-      </Box>
+      <Counter />
     </Container>
   );
 };
---
+++ src/counter.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { useState } from "react";
+import { Box, Typography, IconButton, Button } from "@mui/material";
+
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    localStorage.setItem("countValue", count + 1);
+    setCount(Number(localStorage.getItem("countValue")));
+  };
+  const resetCount = () => {
+    localStorage.setItem("countValue", 0);
+    setCount(0);
+  };
+  const decrementCount = () => {
+    localStorage.setItem("countValue", count > 0 ? count - 1 : 0);
+    setCount(Number(localStorage.getItem("countValue")));
+  };
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Typography variant="h3" gutterBottom>
+        Tally Counter
+      </Typography>
+      <Typography
+        variant="h3"
+        gutterBottom
+        sx={{ margin: "3rem 0 !important", fontWeight: "bold" }}
+      >
+        {Number(localStorage.getItem("countValue"))}
+      </Typography>
+      {/* <Box
+    display="flex"
+    justifyContent="space-between"
+    width="50vw"
+    alignItems="center"
+    marginBottom="1rem"
+  >
+    <IconButton size="large" onClick={incrementCount}>
+      <AddIcon sx={{ fontSize: "6rem" }} />
+    </IconButton>
+    <IconButton size="large" onClick={decrementCount}>
+      <RemoveIcon sx={{ fontSize: "6rem" }} />
+    </IconButton>
+  </Box> */}
+      <Button
+        variant="contained"
+        sx={{ width: "60vw", height: "40vh" }}
+        onClick={incrementCount}
+      >
+        <AddIcon sx={{ fontSize: "6rem" }} />
+      </Button>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        width="50vw"
+        alignItems="center"
+        marginBottom="1rem"
+      >
+        <Button
+          size="large"
+          onClick={resetCount}
+          variant="contained"
+          sx={{ height: "3rem", margin: "3rem 0 !important" }}
+        >
+          Reset
+        </Button>
+        <IconButton size="large" onClick={decrementCount}>
+          <RemoveIcon sx={{ fontSize: "6rem" }} />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Counter;
